913dbc9fec1f1970750cd9a4ac75b41cb4640306
SlavaFors <furtive7@yandex.ru>
2022-07-20 18:34:52 +0300

    final loginPage

---
+++ public/loginPage.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const userForm = new UserForm(); 
+
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login (data, response => {
+        
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.error);
+        }
+    });
+}
+
+
+/*--------------------------------------------*/
+userForm.registerFormCallback = (data) => {
+    console.log(data);
+    ApiConnector.register (data, response => {
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.error);
+        }
+    });
+}
+
